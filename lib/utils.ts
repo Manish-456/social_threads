0c3000ca2dc0b61ab501a5f902ddfe680829195f
Manish-456 <tamangmanish3456@gmail.com>
2023-08-08 15:22:06 +0545

    date fixed

--- lib/utils.ts
+++ lib/utils.ts
@@ -23,7 +23,7 @@ export function formatDateString(dateString: string) {
   const date = new Date(dateString);
   const formattedDate = date.toLocaleDateString(undefined, options);
 
-  const time = date.toLocaleTimeString([], {
+  const time = date.toLocaleTimeString("en-us", {
     hour: "numeric",
     minute: "2-digit",
   });
